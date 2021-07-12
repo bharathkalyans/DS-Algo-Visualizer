e99a025d3b4d1f60444bf8d9666b378b02e5a65a
Bharath Kalyan S <sbharath35@gmail.com>
2021-07-12 19:52:46 +0530

    Completed SetsHomePage Activity! 🤩

--- app/src/main/java/com/bharathkalyans/dsalgovisualizer/sets/SetsHomePage.kt
+++ app/src/main/java/com/bharathkalyans/dsalgovisualizer/sets/SetsHomePage.kt
@@ -1,8 +1,10 @@
 package com.bharathkalyans.dsalgovisualizer.sets
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.bharathkalyans.dsalgovisualizer.databinding.ActivitySetsHomePageBinding
+import java.util.*
 
 class SetsHomePage : AppCompatActivity() {
 
@@ -12,6 +14,49 @@ class SetsHomePage : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         setsbinding = ActivitySetsHomePageBinding.inflate(layoutInflater)
+
+        val hashSet: HashSet<Int> = HashSet<Int>()
+        val treeSet: TreeSet<Int> = TreeSet<Int>()
+
+        val hashStringBuilder: StringBuilder = StringBuilder()
+        val treeStringBuilder: StringBuilder = StringBuilder()
+
+
+        setsbinding.btnAddIntoSets.setOnClickListener {
+
+            val value = setsbinding.etSetsValue.text.toString()
+
+            if (value == "") {
+                Toast.makeText(this, "Please Fill out all Fields 😇", Toast.LENGTH_SHORT).show()
+            } else {
+                if (hashSet.contains(value.toInt())) {
+                    Toast.makeText(this, "Duplicates not Allowed! 🙁", Toast.LENGTH_SHORT).show()
+                } else {
+
+                    //HashSet
+                    if (hashSet.size != 0 && hashSet.size > 7) {
+                        hashStringBuilder.append("\n")
+                    }
+                    hashSet.add(value.toInt())
+                    hashStringBuilder.clear()
+                    hashStringBuilder.append(hashSet.toString())
+
+                    setsbinding.tvCardHashSet.text = hashStringBuilder.toString()
+
+                    //TreeSet
+                    if (treeSet.size != 0 && treeSet.size > 7) {
+                        treeStringBuilder.append("")
+                    }
+                    treeSet.add(value.toInt())
+                    treeStringBuilder.clear()
+                    treeStringBuilder.append(treeSet.toString())
+
+                    setsbinding.tvCardTreeSet.text = treeStringBuilder.toString()
+                }
+            }
+
+
+        }
 
         setContentView(setsbinding.root)
     }
